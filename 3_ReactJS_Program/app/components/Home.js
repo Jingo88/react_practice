e313d4278d78735b81916412420f7af013156600
Jason <Jcory.ng@gmail.com>
2016-04-07 15:41:32 -0400

    long time my friend, finished react router on number 3

---
+++ 3_ReactJS_Program/app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Home = React.createClass({
+	render: function(){
+		return(
+			<div> Hello You are in the Home Component</div>
+		)
+	}
+})
+
+module.exports =  Home;
