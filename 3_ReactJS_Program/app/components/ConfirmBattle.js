0833f6e6c398b6c9071a86aa57152d4902ae69e1
Jason <Jcory.ng@gmail.com>
2016-04-12 17:41:26 -0400

    moar updates to confirm battle

--- 3_ReactJS_Program/app/components/ConfirmBattle.js
+++ 3_ReactJS_Program/app/components/ConfirmBattle.js
@@ -1,5 +1,6 @@
 var React = require("react")
 var PropTypes = React.PropTypes;
+var styles = require('../styles');
 
 function puke(obj){
 	return <pre>{JSON.stringify(obj, null, '')}</pre>
@@ -10,7 +11,29 @@ function puke(obj){
 function ConfirmBattle(props){
 	return props.isLoading === true 
 		? <p> LOADING!!!</p>
-		: <div>CONFIRM BATTLE: {puke(props)}</div>
+		: <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+				<h1> Confirm Players </h1>
+				<div className="col-sm-8 col-sm-offset-2">
+					<div className="col-sm-6">
+						<p className="lead"> Player 1</p>
+							{puke(props.playersInfo[0])}
+					</div>
+					<div className="col-sm-6">
+						<p className="lead"> Player 2 </p>
+							{puke(props.playersInfo[1])}
+					</div>
+				</div>
+
+				<div className="col-sm-8 col-sm-offset-2">
+					<div className="col-sm-12" style={styles.space}>
+						INITIATE BATTLE BUTTON
+					</div>
+
+					<div className="col-sm-12" style={styles.space}>
+						LINK to PLAYER ONE
+					</div> 
+				</div>
+			</div>
 }
 
 ConfirmBattle.propTypes = {
