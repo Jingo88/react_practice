e49e2f1896dcf0f3f12e3e06114e09f98ae13b4e
Jason <Jcory.ng@gmail.com>
2016-04-28 17:52:52 -0400

    added results container and proptypes to results

--- 3_ReactJS_Program/app/components/Results.js
+++ 3_ReactJS_Program/app/components/Results.js
@@ -2,11 +2,22 @@
 var React = require('react')
 var PropTypes = React.PropTypes
 
+function puke(obj){
+	return <pre>{JSON.stringify(obj,2, ' ') } </pre>
+}
+
 function Results(props){
 	return(
 		<div> Results </div>
 	)
 }
+
+Results.propTypes = {
+	isLoading: PropTypes.bool.isRequired,
+	playersInfo: PropTypes.array.isRequired,
+	scores: PropTypes.array.isRequired
+}
+
 
 // Make sure to export the function for use
 module.exports = Results;
