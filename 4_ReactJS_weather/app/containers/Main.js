c102e9aec7a20569e64af67f78e5b5084652577f
Jason <Jcory.ng@gmail.com>
2016-05-04 14:20:49 -0400

    setting up router

---
+++ 4_ReactJS_weather/app/containers/Main.js
@@ -0,0 +1,20 @@
+var React = require('react');
+// var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+	render: function(){
+		return (
+			<div> 
+				<h1>ERRO WARLD </h1>
+				<h4> Time for some weather shit </h4>
+			</div>
+		)
+	}
+})
+
+module.exports = HelloWorld;
+
+// ReactDOM.render(
+// 	<HelloWorld/>,
+// 	document.getElementById('app')
+// )
