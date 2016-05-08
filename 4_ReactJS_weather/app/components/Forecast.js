93b19ae1d56c731e062a255ce6d582df970aee9c
Jason <Jcory.ng@gmail.com>
2016-05-08 14:40:50 -0400

    break down forecast .map even further

--- 4_ReactJS_weather/app/components/Forecast.js
+++ 4_ReactJS_weather/app/components/Forecast.js
@@ -1,6 +1,17 @@
 var React = require('react');
 
 
+function EachDayWeather(props){
+	console.log(props.data)
+	var fiveWeather = props.data
+	return fiveWeather.map(function(day){
+		console.log(day.weather[0].description)
+		
+	})
+		
+	
+}
+
 function FiveDayWeather(props){
 	return (
 		<div>
@@ -12,6 +23,7 @@ function FiveDayWeather(props){
 				<li>Main: {props.data[0].weather[0].main}</li>
 				<li>Humidity: {props.data[0].humidity}</li>
 			</ul>
+			<EachDayWeather data={props.data}/>
 		</div>
 	)
 }
