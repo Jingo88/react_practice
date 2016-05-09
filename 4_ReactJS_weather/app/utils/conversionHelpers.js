f9419502a185a87d07df2bab8f7fe4eb2c2e0211
Jason <Jcory.ng@gmail.com>
2016-05-09 12:12:59 -0400

    got some weather icons, svg style babyyyy

--- 4_ReactJS_weather/app/utils/conversionHelpers.js
+++ 4_ReactJS_weather/app/utils/conversionHelpers.js
@@ -3,6 +3,11 @@ function kelToFar(temp){
 	return (temp * (9/5) - 459.67)
 }
 
+function iconProducer(description){
+
+}
+
 module.exports = {
-	kelToFar : kelToFar
+	kelToFar : kelToFar,
+	iconProducer : iconProducer
 }
