ddbc01de8113369f1848d29dd345a57f1cfdce3c
Jason <Jcory.ng@gmail.com>
2016-05-12 16:22:43 -0400

    bunch of tries with vertical align, did not work, made some comments

--- 4_ReactJS_weather/app/components/Loading.js
+++ 4_ReactJS_weather/app/components/Loading.js
@@ -7,26 +7,28 @@ const styles = {
 	}
 }
 
+//figure out vertical alignment. 
+//materialize has a valign-wrapper and a valign class, was not working the way i wanted
 function Loading(props){
 	return(
 		<div 
 			style={styles.myContainer}
 			className="container">
 			
-			<div className="row">
-				<h1 className="col s12 center"> 
-					Loading
-				</h1>
-			</div>
+					<div className="row">
+						<h1 className="col s12 center"> 
+							Loading
+						</h1>
+					</div>
 
-			<div className="row">
-				<div className="progress col s12 m6 offset-m3">
-      		<div className="indeterminate">
-      			
-      		</div>
-  			</div>
-			</div>
-			
+					<div className="row">
+						<div className="progress col s12 m6 offset-m3">
+		      		<div className="indeterminate">
+		      			
+		      		</div>
+		  			</div>
+					</div>
+
 		</div>
 	)
 }
