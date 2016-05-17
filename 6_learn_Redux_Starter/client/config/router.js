41c012da6f4c5c77e864b7bfb54552c8b712d163
Jason <Jcory.ng@gmail.com>
2016-05-17 15:50:34 -0400

    figure out how to export router later

---
+++ 6_learn_Redux_Starter/client/config/router.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+
+import Main from '../components/Main';
+import PhotoGrid from '../components/PhotoGrid';
+import Single from '../components/Single';
+
+const router = (
+	<Router history={browserHistory}>
+		<Route path="/" component = {Main}>
+			<IndexRoute component={PhotoGrid}></IndexRoute>
+			<Route path="/view/:postId" component={Single}></Route>
+		</Route>
+	</Router>
+)
+
+// Instead of making it a class just make it a functional component
+// const Router = React.createClass({
+// 	render(){
+// 		return(
+
+// 		)
+// 	}
+// })
+
+// export const router;
+// export function router;
+export default router;
--- 6_learn_Redux_Starter/client/reduxstagram.js
+++ 6_learn_Redux_Starter/client/reduxstagram.js
@@ -1,19 +1,40 @@
 //make sure to include es2015 in your presets
 //make sure to npm install babel-preset-es2015
 import React from 'react';
-
 import { render } from 'react-dom';
-
 import css from './styles/style.styl';
 
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
 
 render(
-	<Main>
-		<div>blahblahblah</div>
-	</Main>, 
+	router, 
 	document.getElementById('root')
 );
+
+
+
+
+// import router from './config/router';
+
+// render(
+// 	<router />, 
+// 	document.getElementById('root')
+// );
 
 
 
