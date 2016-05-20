dd7fa7fa06927ba06dead704116ef4fa84ba26cb
Jason <Jcory.ng@gmail.com>
2016-05-20 10:29:14 -0400

    made photo presentational component

--- 6_learn_Redux_Starter/client/components/PhotoGrid.js
+++ 6_learn_Redux_Starter/client/components/PhotoGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
 	render(){
 		return(
 			<h3 className="photo-grid">
-				I am in the PhotoGrid class
+				{this.props.posts.map((post, i) => <Photo />)}
 			</h3>
 		)
 	}
