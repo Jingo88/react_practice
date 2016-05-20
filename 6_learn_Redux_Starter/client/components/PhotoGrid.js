37ca79362fff2050718cdbe37e53b945a1bbdffb
Jason <Jcory.ng@gmail.com>
2016-05-20 11:47:39 -0400

    pulling in photo info for grid population

--- 6_learn_Redux_Starter/client/components/PhotoGrid.js
+++ 6_learn_Redux_Starter/client/components/PhotoGrid.js
@@ -4,9 +4,9 @@ import Photo from './Photo';
 const PhotoGrid = React.createClass({
 	render(){
 		return(
-			<h3 className="photo-grid">
-				{this.props.posts.map((post, i) => <Photo />)}
-			</h3>
+			<div className="photo-grid">
+				{this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
+			</div>
 		)
 	}
 })
