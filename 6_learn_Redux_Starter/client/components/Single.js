54b864544b93e6b98640b7084e0dce250f499a4e
Jason <Jcory.ng@gmail.com>
2016-05-22 15:57:27 -0400

    take time to figure out error in Photo.js file

--- 6_learn_Redux_Starter/client/components/Single.js
+++ 6_learn_Redux_Starter/client/components/Single.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Photo from './Photo';
 
+//findIndex is new to the browser
 const Single = React.createClass({
 	render(){
+		const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+		const post = this.props.posts[i]
+		console.log(i)
+		console.log(post)
 		return(
 			<h3 className="single-photo">
 				I am in the single class
