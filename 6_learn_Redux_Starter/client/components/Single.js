8cdca8290aec8703dfb10900c2f60abb94cb7998
Jason <Jcory.ng@gmail.com>
2016-05-22 16:02:53 -0400

    refactored the single photo view, time to edit comments.js to render UI

--- 6_learn_Redux_Starter/client/components/Single.js
+++ 6_learn_Redux_Starter/client/components/Single.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Photo from './Photo';
+import Comment from './Comments';
 
 //findIndex is new to the browser
 const Single = React.createClass({
 	render(){
 		const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
 		const post = this.props.posts[i]
-		console.log(i)
-		console.log(post)
+		
 		return(
 			<h3 className="single-photo">
-				I am in the single class
+				<Photo 
+					i={i}
+					post = {post}
+					{...this.props}/>
+				<Comment />
 			</h3>
 		)
 	}
