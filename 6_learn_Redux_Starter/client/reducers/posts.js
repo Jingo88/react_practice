8ab6d8468f8de989235bfc6799a477e6841b9a41
Jason <Jcory.ng@gmail.com>
2016-05-23 12:59:56 -0400

    hot loading reducers

--- 6_learn_Redux_Starter/client/reducers/posts.js
+++ 6_learn_Redux_Starter/client/reducers/posts.js
@@ -8,7 +8,11 @@ function posts(state = [], action) {
     case 'INCREMENT_LIKES' :
       console.log("Incrementing Likes!!");
       const i = action.index;
-      return [...state.slice(0,i),{...state[i], likes: state[i].likes + 1},...state.slice(i + 1)];
+      return [
+      	...state.slice(0,i),
+      	{...state[i], likes: state[i].likes + 1},
+      	...state.slice(i + 1)
+      ];
     default:
       return state;
   }
