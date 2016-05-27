e310cdd66a0c989d67abc40da3e34b7a5d2b4676
Jason <Jcory.ng@gmail.com>
2016-05-27 16:51:35 -0400

    set up materialize card reveal with jquery

--- 5_React_Movie_Lister/app/components/MovieList.js
+++ 5_React_Movie_Lister/app/components/MovieList.js
@@ -24,12 +24,26 @@ function MovieUI(props){
 				</div>
 
 				<div className="card-content">
-		      <span className="card-title activator grey-text text-darken-4">{props.data.Title}<i className="material-icons right">more_vert</i></span>
-		      <p><a href="#">This is a link</a></p>
+		      <span className="card-title activator grey-text text-darken-4">
+		      	{props.data.Title}
+		      	<i className="material-icons right">more_vert</i>
+		      </span>
+				</div>
+
+				<div className="card-action">
+					<a href="#">Add to favorites</a>
+					<a href="#">Share</a>
 				</div>
 
 				<div className="card-reveal">
-      		<span className="card-title grey-text text-darken-4">{props.data.Title}<i className="material-icons right">close</i></span>
+      		<span className="card-title grey-text text-darken-4">
+      			{props.data.Title}
+      			<ul>
+      				<li>{props.data.Year}</li>
+      				<li>{props.data.Type}</li>
+      			</ul>
+      			<i className="material-icons right">close</i>
+      		</span>
     		</div>
 			</div>
 		</div>
