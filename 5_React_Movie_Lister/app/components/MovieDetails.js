4d2ba9ee0b3b7b6224b36d2c8e576c17b65348bd
Jason <Jcory.ng@gmail.com>
2016-05-30 16:18:41 -0400

    loading circle

--- 5_React_Movie_Lister/app/components/MovieDetails.js
+++ 5_React_Movie_Lister/app/components/MovieDetails.js
@@ -4,13 +4,23 @@ function MovieDetails(props){
 	
 	let movieInfo = props.movieInfo.data
 	console.log(movieInfo)
+
 	return props.loading === true
 		? <div className="card-reveal">
   			<span className="card-title grey-text text-darken-4">
-	  			{props.movie.Title}
-	  			{props.movie.Year}
-	  			{props.movie.Type}
-  			
+	  			<div className="preloader-wrapper big active">
+			      <div className="spinner-layer spinner-red">
+			        <div className="circle-clipper left">
+			          <div className="circle"></div>
+			        </div>
+			        <div className="gap-patch">
+			          <div className="circle"></div>
+			        </div>
+			        <div className="circle-clipper right">
+			          <div className="circle"></div>
+		        	</div>
+		      	</div>
+		      </div>
   			<i className="material-icons right">close</i>
   			</span>
 			</div>
@@ -20,6 +30,7 @@ function MovieDetails(props){
   			<i className="material-icons right">close</i>
   			</span>
   			<ul>
+  					<li>Genre: {movieInfo.Genre}</li>
 	  				<li>Rated: {movieInfo.Rated}</li>
 	  				<li>Year: {movieInfo.Year}</li>
 	  				<li>Released Date: {movieInfo.Released}</li>
@@ -28,6 +39,9 @@ function MovieDetails(props){
 	  				<li>Type: {movieInfo.Type}</li>
 	  			</ul>
 	  			<p>{movieInfo.Plot}</p>
+	  			<p>{movieInfo.Actors}</p>
+	  			<p>{movieInfo.Writer}</p>
+	  			<p>{movieInfo.Director}</p>
 			</div>
 		
 }
